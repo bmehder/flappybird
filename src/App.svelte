68f362841ef80e9833ca4ca2318484926dfdf553
bmehder <bmehder@gmail.com>
2022-07-22 12:28:48 +0100

    made predicated for collision

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
   const flyAudio = new Audio()
   const scoreAudio = new Audio()
 
-  const pipe = []
+  const pipes = []
   const GRAVITY = 1.5
   const GAP = 85
 
@@ -21,14 +21,14 @@
   flyAudio.src = 'sounds/fly.mp3'
   scoreAudio.src = 'sounds/score.mp3'
 
-  let bY = 150
-  let bX = 10
+  let birdX = 10
+  let birdY = 150
   let score = 0
 
   const init = node => {
     const ctx = node.getContext('2d')
 
-    pipe[0] = {
+    pipes[0] = {
       x: node.width,
       y: 0,
     }
@@ -37,9 +37,9 @@
       const yOffset = pipeNorth.height + GAP
       const drawBackground = () => ctx.drawImage(background, 0, 0)
       const drawForeground = () => ctx.drawImage(foreground, 0, node.height - foreground.height)
-      const drawBird = () => ctx.drawImage(bird, bX, bY)
+      const drawBird = () => ctx.drawImage(bird, birdX, birdY)
       const startOver = () => location.reload()
-      const moveBirdDown = () => (bY += GRAVITY)
+      const moveBirdDown = () => (birdY += GRAVITY)
       const displayScore = () => {
         ctx.fillStyle = '#000'
         ctx.font = '20px Monospace'
@@ -50,7 +50,7 @@
         scoreAudio.play()
       }
       const makeNewPipe = () => {
-        pipe.push({
+        pipes.push({
           x: node.width,
           y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height,
         })
@@ -58,27 +58,39 @@
 
       drawBackground()
 
-      for (let i = 0; i < pipe.length; i++) {
-        const isBirdPastPipe = pipe[i].x === 5
-        const isReadyForNewPipe = pipe[i].x === 125
+      for (let i = 0; i < pipes.length; i++) {
+        const isBirdPastPipe = pipes[i].x === 5
+        const isReadyForNewPipe = pipes[i].x === 125
 
-        const isCollision =
-          (bX + bird.width >= pipe[i].x &&
-            bX <= pipe[i].x + pipeNorth.width &&
-            (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + yOffset)) ||
-          bY + bird.height >= node.height - foreground.height
+        const isRightOfBirdTouchingLeftOfTopPipe = birdX + bird.width >= pipes[i].x
+        const isLeftOfBirdTouchingBottomOfTopPipe = birdX <= pipes[i].x + pipeNorth.width
+        const isTopOfBirdTouchingBottomOfTopPipe = birdY <= pipes[i].y + pipeNorth.height
+        const isBottomOfBirdTouchingTopOfBottomPipe = birdY + bird.height >= pipes[i].y + yOffset
+        const isFloorCollision = birdY + bird.height >= node.height - foreground.height
 
-        const drawPipeNorth = () => ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y)
-        const drawPipeSouth = () => ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + yOffset)
+        // const isPipeCollision =
+        //   (isRightOfBirdTouchingLeftOfTopPipe &&
+        //     isLeftOfBirdTouchingBottomOfTopPipe &&
+        //     (isTopOfBirdTouchingBottomOfTopPipe || isBottomOfBirdTouchingTopOfBottomPipe)) ||
+        //   isFloorCollision
+        const isPipeCollision =
+          isRightOfBirdTouchingLeftOfTopPipe &&
+          isLeftOfBirdTouchingBottomOfTopPipe &&
+          (isTopOfBirdTouchingBottomOfTopPipe || isBottomOfBirdTouchingTopOfBottomPipe)
 
-        const movePipeLeft = () => pipe[i].x--
+        const drawPipeNorth = () => ctx.drawImage(pipeNorth, pipes[i].x, pipes[i].y)
+        const drawPipeSouth = () => ctx.drawImage(pipeSouth, pipes[i].x, pipes[i].y + yOffset)
+
+        const movePipeLeft = () => pipes[i].x--
 
         drawPipeNorth()
         drawPipeSouth()
         movePipeLeft()
 
         isReadyForNewPipe && makeNewPipe()
-        isCollision && startOver()
+        if (isPipeCollision || isFloorCollision) {
+          startOver()
+        }
         isBirdPastPipe && increaseScore()
       }
 
@@ -92,7 +104,7 @@
     draw()
   }
 
-  const handleKeydown = e => (bY -= 25) && flyAudio.play()
+  const handleKeydown = e => (birdY -= 25) && flyAudio.play()
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
